9d0961b9b2c741093fcd54b3dc9272525255e62f
ersalanelang <ersa.kusuma.ek@gmail.com>
2025-07-30 04:13:47 +0700

    Fix vercel error - zod number companionform - run dev4

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
